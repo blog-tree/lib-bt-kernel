904fe8b571e45ef23e7f8818671f0643b593b8e3
Alexander Zagovorichev <zagovorichev@1pt.com>
2016-04-05 12:27:08 +0300

    construct arch

--- Garden/Foundation/Console/ServeCommand.php
+++ Garden/Foundation/Console/ServeCommand.php
@@ -1,5 +1,12 @@
 <?php
-
+/**
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MIT Public License for more details.
+ *
+ * Copyright (c) 2016 (original work) Blog-Tree.com;
+ */
 
 namespace bt\Garden\Foundation\Console;
 
@@ -34,5 +41,4 @@ class ServeCommand extends Command
         
         passthru("{$binary} -S {$host}:{$port} {$base}/server.php");
     }
-    
 }
